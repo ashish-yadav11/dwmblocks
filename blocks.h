ae59888015f857cd1d9e91286598934c584e395c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-07-14 01:26:52 +0530

    Change signal for time block and reordered comments

--- blocks.h
+++ blocks.h
@@ -3,11 +3,11 @@
 
 #define PATH(name)			"/home/ashish/.local/projects/dwmblocks/blocks/"name
 
-/* If multiple realtime signals are pending, then the lowest numbered signal is delivered first.
- * If interval of a block is set to 0, the block will only be updated once at startup.
+/* If interval of a block is set to 0, the block will only be updated once at startup.
  * If interval is set to a negative value, the block will never be updated in the main loop.
  * Set signal to 0 if signalling is not required for the block.
- * Keep signal for clickable blocks less than 10. */
+ * Keep signal for clickable blocks less than 10.
+ * If multiple realtime signals are pending, then the lowest numbered signal is delivered first. */
 
 /* pathu - path of the program whose output is to be used for status text
  * patch - path of the program to be executed on clicks */
@@ -15,7 +15,7 @@ static Block blocks[] = {
 /*	pathu				pathc					interval	signal */
 	{ PATH("hotspot.sh"),		PATH("hotspot_button.sh"),		-1,		3},
 
-	{ PATH("time.sh"),		NULL,					30,		6},
+	{ PATH("time.sh"),		NULL,					30,		10},
 
 	{ PATH("calendar.sh"),		PATH("calendar_button.sh"),		60,		6},
 
