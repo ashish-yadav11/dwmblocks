52535a44f5e2e50509e97d3bf39353c50932266a
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-12 00:52:58 +0530

    Improved block _button scripts

--- blocks/cpu_temp_button.sh
+++ blocks/cpu_temp_button.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case $1 in
-    1) exec termite -e "htop -s PERCENT_CPU" ;;
-    2) exec termite -e "htop" ;;
-    3) exec termite -e "htop -s PERCENT_MEM" ;;
+    1) exec "$TERMINAL" -e "htop -s PERCENT_CPU" ;;
+    2) exec "$TERMINAL" -e "htop" ;;
+    3) exec "$TERMINAL" -e "htop -s PERCENT_MEM" ;;
 esac
